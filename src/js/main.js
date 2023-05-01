8c17efe519d70fd9f038c7ab9fdd0052a9118ada
Tom Eloe <tomeloe1@gmail.com>
2023-05-01 11:39:23 -0700

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const mobileMenuBtn = document.getElementById('mobile-menu-btn');
-    const mainNav = document.getElementById('main-nav');
-    const menuWrapper = document.querySelector('.menu-wrapper');
-    const menu = document.querySelector('.menu');
+
+    const menu = document.querySelector(".menu");
+    const menuBtn = document.getElementById("mobile-menu-btn");
+    const mainNav = document.getElementById("nav-main");
+    const overlay = document.querySelector(".overlay");
+    const body = document.querySelector("body");
 
 
-    mobileMenuBtn.addEventListener('click', () => {
-        mobileMenuBtn.classList.toggle('open');
-        menuWrapper.classList.toggle('open');
+    function toggleMenu() {
+        body.classList.toggle('fixed');
+        menu.classList.toggle("open");
+        this.classList.toggle("open");
+        mainNav.classList.toggle("open");
+
+        if (!menu.classList.contains("open")) {
+            overlay.classList.remove("open");
+        }
+    }
+    function closeMenu() {
+        body.classList.remove('fixed');
+        mainNav.classList.remove("open");
+        menu.classList.remove("open");
+        menuBtn.classList.remove("open");
+        overlay.classList.remove("open");
+    }
+
+    document.addEventListener("keyup", function (e) {
+        if (e.key === "Escape") closeMenu();
     });
+
+    document.addEventListener("click", function () {
+        if (menu.classList.contains("open")) {
+            overlay.classList.add("open");
+        }
+    });
+
+    menuBtn.addEventListener("click", toggleMenu);
+    overlay.addEventListener("click", closeMenu);
+
+    // Always fire closeMenu() at 1025px breakpoint
+    const mq = window.matchMedia("screen and (min-width: 1025px)");
+
+    mq.addEventListener("change", event => {
+        if (event.matches) {
+            closeMenu();
+        }
+    })
+
+
+
+
+
+
 
     // Stop any animation/transition while window is being resized
     let resizeTimer;
